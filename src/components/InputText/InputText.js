c038154010eac16e5be9d183dd5214a2002d77f9
srinibas <srinibaskhuntia39@gmail.com>
2022-05-17 16:51:45 +0530

    Input Text componenet UI done

---
+++ src/components/InputText/InputText.js
@@ -0,0 +1,28 @@
+import "./InputText.css"
+
+export const InputText = () => {
+    return <div className="input-text-container">
+        <div className="input-text">
+
+            <img src={require("../../images/Conor.png")} alt="user" />
+
+            <div className="input-text-text">
+                <input />
+                <div className="input-text-option">
+                    <div>
+                        <span class="material-symbols-outlined">
+                            image
+                        </span>
+                        <span class="material-symbols-outlined">
+                            gif
+                        </span>
+                        <span class="material-symbols-outlined">
+                            mood
+                        </span>
+                    </div>
+                    <button>POST</button>
+                </div>
+            </div>
+        </div>
+    </div>
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import { AutoComplete } from "../components/AutoComplete/AutoComplete"
 import { Home } from "../components/Home/Home"
+import { InputText } from "../components/InputText/InputText"
 import { Navbar } from "../components/NavBar/NavBar"
 
 export const HomePage = () => {
     return <>
         <Navbar />
         <AutoComplete />
-        <Home />
+        {/* <Home /> */}
+        <InputText/>
+
     </>
 }
