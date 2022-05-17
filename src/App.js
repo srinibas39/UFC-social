bfaa5e8111ba05a5fbb3b59c9c97682408f90206
srinibas <srinibaskhuntia39@gmail.com>
2022-05-17 15:15:52 +0530

    Homepage implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/Home/Home";
+import { HomePage } from "./pages/HomePage";
 
 export const App = () => {
-  return <>
-    <Home />
-  </>
+  return <Routes>
+
+    <Route path="/" element={<HomePage />} />
+
+  </Routes>
+
+
 }
 
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { AutoComplete } from "../components/AutoComplete/AutoComplete"
+import { Home } from "../components/Home/Home"
+import { Navbar } from "../components/NavBar/NavBar"
+
+export const HomePage = () => {
+    return <>
+        <Navbar />
+        <AutoComplete />
+        <Home />
+    </>
+}
