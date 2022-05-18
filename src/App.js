1ac4a1721d52629a36b4240f125a31b977631af9
srinibas <srinibaskhuntia39@gmail.com>
2022-05-18 18:29:27 +0530

    Error 404 page added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { HomePage } from "./pages/HomePage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { SignupPage } from "./pages/SignupPage";
 import { LoginPage } from "./pages/LoginPage";
+import { Error } from "./components/Error/Error";
 
 export const App = () => {
   return <Routes>
 
+    <Route path="*" element={<Error />} />
     <Route path="/" element={<HomePage />} />
     <Route path="/profile" element={<ProfilePage />} />
     <Route path="/explore" element={<ExplorePage />} />
