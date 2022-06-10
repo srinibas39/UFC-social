50782a4feece823e0c83a34fd3546d3be46caab1
srinibas <srinibaskhuntia39@gmail.com>
2022-06-10 15:40:28 +0530

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./Store/store.js";
+
 
 
 
