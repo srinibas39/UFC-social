8f2b60934c0d94477c6800b36769424df5adee74
srinibas <srinibaskhuntia39@gmail.com>
2022-06-10 16:06:38 +0530

    bugs fixed

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import postsReducer from "../features/postsSlice"
 import userReducer from "../features/userSlice";
+// import userReducer from "../features/userSlice";
+
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         posts: postsReducer,
         users: userReducer
+        
     },
 
 })
